dea615fc1aa5deee1f2b6314e03aa410d1d6162b
Eric Chrobak <eric@ericchrobak.com>
2018-11-27 01:41:52 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,13 +12,13 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(username: params["username"],password: params["password"])
-    binding.pry
+    # binding.pry
     session[:user_id] = @user.id
     redirect '/account'
   end
 
   get '/account' do
-    binding.pry
+    # binding.pry
     if session[:user_id] == "" || session[:user_id] == nil
       erb :error
     else
